806ee49d0bd68034aacf4a700011b03b532f517a
Rogelio <rogercp1234@yahoo.com>
2019-10-28 10:17:19 -0700

    created users table

--- data/migrations/20191025221607_users.js
+++ data/migrations/20191025221607_users.js
@@ -1,8 +1,11 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema.createTable("users", table=>{
+      table.increment();
+
+  })
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists("users")
 };
