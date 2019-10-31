35f741f983cb9c52cb8a8bf44d79b418e8b3ad23
Rogelio <rogercp1234@yahoo.com>
2019-10-30 20:28:06 -0700

    created user model and controller

---
+++ server/models/User.js
@@ -0,0 +1,27 @@
+'use strict'
+
+/**
+ * Dependencies
+ */
+
+const db = require('../../data/dbConfig')
+
+/**
+ * Define model
+ */
+
+ class User{
+
+    static all(){
+        return db('users')
+    }
+
+
+
+ }
+
+ /**
+ * Export model
+ */
+
+module.exports = User
---
+++ server/routes/users_router.js
@@ -0,0 +1,44 @@
+'use strict'
+
+/**
+ * Dependencies
+ */
+
+const express = require('express');
+const UsersController = require('../controllers/UsersController');
+const restricted = require('../middleware/restricted');
+const adminAuth = require('../middleware/adminAuth');
+const require_body = require('../middleware/require_body');
+
+/**
+ * Define router
+ */
+
+const router = express.Router()
+
+/**
+ * Routes
+ *   GET /users
+ */
+
+router.route('/')
+  .get(UsersController.index)
+
+/**
+ * Routes
+ *   POST /users/auth
+ */
+
+
+/**
+ * Routes
+ *   PUT /users/deactivate
+ */
+
+
+/*
+/**
+ * Export router
+ */
+
+module.exports = router
