54698c8c41f60552d6a2931e4bed0a40e86095ea
Rogelio <rogercp1234@yahoo.com>
2019-11-13 22:09:18 -0800

    created new controllers and models for car and car fixes

---
+++ server/controllers/CarFixController.js
@@ -0,0 +1,19 @@
+'use strict'
+
+/**
+ * Define controller
+ */
+
+class CarFixController{
+    static index(req, res){
+        
+    }
+
+}
+  
+  /**
+   * Export controller
+   */
+
+   module.exports = CarFixController
+  
---
+++ server/routes/cars_router.js
@@ -0,0 +1,44 @@
+'use strict'
+
+/**
+ * Dependencies
+ */
+
+const express = require('express');
+const CarsController = require('../controllers/CarsController.js');
+const restricted_access = require('../middleware/restricted_access');
+// const require_body = require('../middleware/require_body');
+
+/**
+ * Define router
+ */
+
+const router = express.Router()
+
+/**
+ * Routes
+ *   GET 
+ */
+
+
+
+/**
+ * Routes
+ *   POST 
+ */
+
+
+
+
+/**
+ * Routes
+ *   PUT 
+ */
+
+
+/*
+/**
+ * Export router
+ */
+
+module.exports = router
