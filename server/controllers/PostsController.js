40957f5ba5a300cda0d9927dc6b4608903be34ae
Rogelio <rogercp1234@yahoo.com>
2020-01-10 18:58:33 -0800

    created controller and model functions for posts

--- server/controllers/PostsController.js
+++ server/controllers/PostsController.js
@@ -8,6 +8,57 @@ const Post = require('../models/Post')
 
 class PostsController{
 
+   
+  static async allUserPosts(req, res) {
+    try {
+      const posts = await Post.allUserPosts();
+
+      return res.status(200).json(posts);
+    } catch (err) {
+      return res
+        .status(500)
+        .json({ error: { message: "Internal Server Error" } });
+    }
+  }
+
+  static async allPosts(req, res) {
+    try {
+      const posts = await Post.all();
+
+      return res.status(200).json(posts);
+    } catch (err) {
+      return res
+        .status(500)
+        .json({ error: { message: "Internal Server Error" } });
+    }
+  }
+
+
+
+    static async create(req, res) {
+    
+        try{
+          
+          const post = await Post.create(req.body,req.params);
+    
+          res.status(201).json(post);
+        }catch(err){
+          return res
+          .status(500)
+          .json({ error: { message: "Internal Server Error" } });
+        }
+        }
+
+    static async delete(req, res) {
+            try {
+              await Post.delete(req.params.id, req.body.email);
+        
+              return res.status(200).json({ message: "Successfully deleted post" });
+            } catch (err) {
+              return res.status(500).json({ error: { message: "Internal Server Error" } });
+            }
+          }
+       
 }
   
   /**
